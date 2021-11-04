3fffcd815cbe8e4a73ded86a654a96797961eb14
Vadim <rvdagi05@yandex.ru>
2021-11-04 10:14:01 +0300

    Choose image

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -21,6 +21,29 @@ class NewPlaceViewController: UITableViewController {
         //скрывать клавиатуру при нажатии на ячейку
         if indexPath.row == 0 {
             
+            //меню для установки изображения
+            let actionSheep = UIAlertController(title: nil,
+                                                message: nil,
+                                                preferredStyle: .actionSheet)
+            
+            //действие для алерта: изображение из камеры
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            
+            //действие для алерта: изображение из библиотеки
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheep.addAction(camera)
+            actionSheep.addAction(photo)
+            actionSheep.addAction(cancel)
+            
+            present(actionSheep, animated: true)
+            
         } else {
             view.endEditing(true)
         }
@@ -37,4 +60,25 @@ extension NewPlaceViewController: UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
+}
+
+// MARK: Work with image
+
+extension NewPlaceViewController {
+    
+    ///Выбор изображения из указанного источника
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        
+        //проверка доступности источника изображения
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            //возможность редактировать изображение
+            imagePicker.allowsEditing = true
+            //указываем тип источника изображения
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+        
+    }
+    
 }
